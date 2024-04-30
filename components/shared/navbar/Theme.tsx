89753a2715670cd02e3ca8d05e0aebedec637f4c
hcui3 <youremail@example.com>
2024-04-30 09:52:28 -0400

    add homepage

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -13,6 +13,7 @@ import { themes } from "@/constants";
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
+
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
@@ -35,14 +36,13 @@ const Theme = () => {
             />
           )}
         </MenubarTrigger>
-        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border bg-light-900 py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
             <MenubarItem
-              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
               key={item.value}
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
               onClick={() => {
                 setMode(item.value);
-
                 if (item.value !== "system") {
                   localStorage.theme = item.value;
                 } else {
